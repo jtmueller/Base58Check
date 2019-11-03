8209a2d60ed190f1b4647587269d6c431055414b
Joel Mueller <joel.mueller@gmail.com>
2019-11-03 17:30:28 -0600

    Support for UTF-8 byte spans, writing to externally-allocated spans.

--- src/Base58Check/Base58CheckEncoding.cs
+++ src/Base58Check/Base58CheckEncoding.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Buffers;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Base58Check
 {
@@ -16,6 +18,9 @@ namespace Base58Check
         private const int CHECKSUM_SIZE = 4;
         private const int HASH_BYTES = 32;
         private const string DIGITS = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        private static readonly byte[] DIGIT_BYTES = Encoding.UTF8.GetBytes(DIGITS);
+
+        // TODO: Change the API to allow callers to allocate destination? Might need a "MaxByte/MaxChars" method.
 
         /// <summary>
         /// Encodes data with a 4-byte checksum
@@ -24,11 +29,40 @@ namespace Base58Check
         /// <returns></returns>
         public static string EncodeWithChecksum(ReadOnlySpan<byte> data)
         {
-            Span<byte> dataWithChecksum = data.Length > 100 
-                ? new byte[data.Length + CHECKSUM_SIZE] 
-                : stackalloc byte[data.Length + CHECKSUM_SIZE];
-            _ = AddCheckSum(data, dataWithChecksum);
-            return EncodePlain(dataWithChecksum);
+            byte[] result = ArrayPool<byte>.Shared.Rent(MaxCharsWithChecksum(data.Length));
+            try
+            {
+                int written = EncodeWithChecksum(data, result);
+                return Encoding.UTF8.GetString(result.AsSpan(..written));
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(result);
+            }
+        }
+
+        /// <summary>
+        /// Encodes data with a 4-byte checksum.
+        /// Writes UTF-8 bytes to the destination span.
+        /// </summary>
+        /// <param name="data">Data to be encoded</param>
+        /// <returns></returns>
+        public static int EncodeWithChecksum(ReadOnlySpan<byte> data, Span<byte> destination)
+        {
+            byte[]? pooled = data.Length > 100 ? ArrayPool<byte>.Shared.Rent(data.Length + CHECKSUM_SIZE) : null;
+            try
+            {
+                Span<byte> dataWithChecksum = pooled ?? stackalloc byte[data.Length + CHECKSUM_SIZE];
+                _ = AddCheckSum(data, dataWithChecksum);
+                return EncodePlain(dataWithChecksum, destination);
+            }
+            finally
+            {
+                if (pooled != null)
+                {
+                    ArrayPool<byte>.Shared.Return(pooled);
+                }
+            }
         }
 
         /// <summary>
@@ -41,6 +75,29 @@ namespace Base58Check
             if (data.Length == 0)
                 return string.Empty;
 
+            byte[] result = ArrayPool<byte>.Shared.Rent(MaxChars(data.Length));
+            try
+            {
+                int written = EncodePlain(data, result);
+                return Encoding.UTF8.GetString(result.AsSpan(..written));
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(result);
+            }
+        }
+
+        /// <summary>
+        /// Encodes data in plain Base58, without any checksum.
+        /// Writes UTF-8 bytes to the destination span.
+        /// </summary>
+        /// <param name="data">The data to be encoded</param>
+        /// <returns>Returns the number of bytes written to the destination span.</returns>
+        public static int EncodePlain(ReadOnlySpan<byte> data, Span<byte> destination)
+        {
+            if (data.Length == 0)
+                return 0;
+
             // Decode bytes to BigInteger
             var intData = BigInteger.Zero;
             for (int i = 0; i < data.Length; i++)
@@ -48,49 +105,47 @@ namespace Base58Check
                 intData = (intData << 8) + data[i];
             }
 
-            var digits = DIGITS.AsSpan();
             var fiftyEight = new BigInteger(58);
+            byte one = (byte)'1';
 
-            // Encode BigInteger to Base58 string
+            // Encode BigInteger to Base58 char bytes
             int pos = 0;
-            var result = ArrayPool<char>.Shared.Rent((int)(data.Length * (5.0 / 3.0)));
-            try
-            {
-                while (intData > BigInteger.Zero)
-                {
-                    intData = BigInteger.DivRem(intData, fiftyEight, out var remainder);
-                    result[pos++] = digits[(int)remainder];
-                }
 
-                // Append `1` for each leading 0 byte
-                for (int i = 0; i < data.Length && data[i] == 0; i++)
-                {
-                    result[pos++] = '1';
-                }
-
-                return string.Create(pos, result, (chars, state) =>
-                {
-                    int len = chars.Length;
-                    for (int i = 0; i < len; i++)
-                    {
-                        chars[i] = state[len - i - 1];
-                    }
-                });
-            }
-            finally
+            while (intData > BigInteger.Zero)
             {
-                ArrayPool<char>.Shared.Return(result);
+                intData = BigInteger.DivRem(intData, fiftyEight, out var remainder);
+                destination[pos++] = DIGIT_BYTES[(int)remainder];
             }
+
+            // Append `1` for each leading 0 byte
+            for (int i = 0; i < data.Length && data[i] == 0; i++)
+            {
+                destination[pos++] = one;
+            }
+
+            destination[..pos].Reverse();
+
+            return pos;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int MaxChars(int byteCount) => (int)Math.Ceiling(byteCount * (5.0 / 3.0));
+
+        public static int MaxCharsWithChecksum(int byteCount) => MaxChars(byteCount + CHECKSUM_SIZE);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int MaxBytes(int charCount) => (int)Math.Ceiling(charCount / (5.0 / 3.0));
+
+        public static int MaxBytesWithChecksum(int charCount) => MaxBytes(charCount) - CHECKSUM_SIZE;
 
         /// <summary>
         /// Decodes data in Base58Check format (with 4 byte checksum)
         /// </summary>
-        /// <param name="data">Data to be decoded</param>
+        /// <param name="chars">Data to be decoded</param>
         /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
-        public static ReadOnlySpan<byte> DecodeWithChecksum(ReadOnlySpan<char> data)
+        public static ReadOnlySpan<byte> DecodeWithChecksum(ReadOnlySpan<char> chars)
         {
-            var dataWithCheckSum = DecodePlain(data);
+            var dataWithCheckSum = DecodePlain(chars);
             var dataWithoutCheckSum = VerifyAndRemoveCheckSum(dataWithCheckSum);
 
             if (dataWithoutCheckSum.IsEmpty)
@@ -99,6 +154,42 @@ namespace Base58Check
             }
 
             return dataWithoutCheckSum;
+        }
+
+        /// <summary>
+        /// Decodes data in Base58Check format (with 4 byte checksum)
+        /// </summary>
+        /// <param name="chars">Data to be decoded</param>
+        /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
+        public static int DecodeWithChecksum(ReadOnlySpan<char> chars, Span<byte> destination)
+        {
+            var bytesDecoded = DecodePlain(chars, destination);
+            var dataWithoutCheckSum = VerifyAndRemoveCheckSum(destination[..bytesDecoded]);
+
+            if (dataWithoutCheckSum.IsEmpty)
+            {
+                throw new FormatException("Base58 checksum is invalid.");
+            }
+
+            return dataWithoutCheckSum.Length;
+        }
+
+        /// <summary>
+        /// Decodes data in Base58Check format (with 4 byte checksum)
+        /// </summary>
+        /// <param name="data">Data to be decoded</param>
+        /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
+        public static int DecodeWithChecksum(ReadOnlySpan<byte> chars, Span<byte> destination)
+        {
+            var bytesDecoded = DecodePlain(chars, destination);
+            var dataWithoutCheckSum = VerifyAndRemoveCheckSum(destination[..bytesDecoded]);
+
+            if (dataWithoutCheckSum.IsEmpty)
+            {
+                throw new FormatException("Base58 checksum is invalid.");
+            }
+
+            return dataWithoutCheckSum.Length;
         }
 
         /// <summary>
@@ -145,6 +236,82 @@ namespace Base58Check
             if (intData.TryWriteBytes(result.AsSpan(leadingZeroCount..), out int _, isUnsigned: true, isBigEndian: true))
             {
                 return result;
+            }
+
+            throw new FormatException("Unable to decode the given Base58 string.");
+        }
+
+        /// <summary>
+        /// Decodes data in plain Base58 (as a UTF-8 byte span), without any checksum.
+        /// Writes the decoded bytes to the destination span.
+        /// </summary>
+        /// <param name="data">Data to be decoded</param>
+        /// <returns>Returns the number of bytes written to the destination span</returns>
+        public static int DecodePlain(ReadOnlySpan<char> chars, Span<byte> destination)
+        {
+            var maxByteCount = Encoding.UTF8.GetMaxByteCount(chars.Length);
+            var pooled = maxByteCount > 100 ? ArrayPool<byte>.Shared.Rent(maxByteCount) : null;
+            try
+            {
+                Span<byte> bytes = pooled ?? stackalloc byte[maxByteCount];
+                int written = Encoding.UTF8.GetBytes(chars, bytes);
+                return DecodePlain(bytes[..written], destination);
+            }
+            finally
+            {
+                if (pooled != null)
+                    ArrayPool<byte>.Shared.Return(pooled);
+            }
+        }
+
+        /// <summary>
+        /// Decodes data in plain Base58 (as a UTF-8 byte span), without any checksum.
+        /// Writes the decoded bytes to the destination span.
+        /// </summary>
+        /// <param name="data">Data to be decoded</param>
+        /// <returns>Returns the number of bytes written to the destination span</returns>
+        public static int DecodePlain(ReadOnlySpan<byte> data, Span<byte> destination)
+        {
+            if (data.Length == 0)
+                return 0;
+
+            var digits = DIGIT_BYTES.AsSpan();
+            var fiftyEight = new BigInteger(58);
+            byte one = (byte)'1';
+
+            // Decode Base58 string to BigInteger 
+            BigInteger intData = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                int digit = digits.IndexOf(data[i]);
+
+                if (digit < 0)
+                {
+                    throw new FormatException($"Invalid Base58 character '{(char)data[i]}' at position {i}.");
+                }
+
+                intData = intData * fiftyEight + digit;
+            }
+
+            // Encode BigInteger to byte[]
+            // Leading zero bytes get encoded as leading `1` characters
+            int leadingZeroCount = 0;
+            for (int i = 0; i < data.Length && data[i] == one; i++)
+                leadingZeroCount++;
+
+            if (leadingZeroCount > 0)
+            {
+                destination[..leadingZeroCount].Clear();
+            }
+
+            if (intData.IsZero)
+            {
+                return leadingZeroCount;
+            }
+
+            if (intData.TryWriteBytes(destination[leadingZeroCount..], out int written, isUnsigned: true, isBigEndian: true))
+            {
+                return written + leadingZeroCount;
             }
 
             throw new FormatException("Unable to decode the given Base58 string.");
